e24304f71aa5b206cc4ba5bd119d8b53b008ab95
lanka-ito <rainbow.knitworks@gmail.com>
2024-12-11 00:06:48 +0900

    kadai_mdn_jsアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear(); //年の取得
+
+const month = today.getMonth() + 1; //月の取得
+
+const day = today.getDate(); //日付の取得
+
+console.log(year + '年' + month + '月' + day + '日' );
